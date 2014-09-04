10b207465e5a05ec6ebf72b978a875e7de7cfac6
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-04 20:04:58 +0300

    Update

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM simpledrupalcloud/base-image
+
+MAINTAINER Simple Drupal Cloud <support@simpledrupalcloud.com>
+
+ENV DEBIAN_FRONTEND noninteractive
+
+ADD ./build /tmp/build
+
+RUN chmod +x /tmp/build/build.sh
+RUN /tmp/build/build.sh
+RUN rm -rf /tmp/*
+
+VOLUME ["/var/www"]
+
+EXPOSE 80
+
+ENTRYPOINT ["/run.sh"]
---
+++ build/etc/puppet/manifests/build.pp
@@ -0,0 +1,72 @@
+class packages {
+  package {[
+      'apache2',
+      'libapache2-mod-fastcgi'
+    ]:
+    ensure => present
+  }
+}
+
+class apache2_supervisor {
+  file { '/etc/supervisor/conf.d/apache2.conf':
+    ensure => present,
+    source => '/tmp/build/etc/supervisor/conf.d/apache2.conf'
+  }
+}
+
+class apache2 {
+  include apache2_supervisor
+
+  file { '/var/www/index.html':
+    ensure => absent
+  }
+
+  file { '/etc/apache2/sites-enabled/000-default':
+    ensure => absent
+  }
+
+  file { '/etc/apache2/sites-available/default-ssl':
+    ensure => absent
+  }
+
+  file { '/etc/apache2/sites-available/default':
+    ensure => present,
+    source => '/tmp/build/etc/apache2/sites-available/default',
+    mode => 644
+  }
+
+  file { '/etc/apache2/sites-enabled/default':
+    ensure => link,
+    target => '/etc/apache2/sites-available/default'
+  }
+
+  exec { '/bin/bash -c "a2enmod actions"': }
+  exec { '/bin/bash -c "a2enmod fastcgi"': }
+  exec { '/bin/bash -c "a2enmod vhost_alias"': }
+  exec { '/bin/bash -c "a2enmod rewrite"': }
+}
+
+node default {
+  file { '/run.sh':
+    ensure => present,
+    source => '/tmp/build/run.sh',
+    mode => 755
+  }
+
+  include packages
+  include apache2
+
+  Class['packages'] -> Class['apache2']
+
+  file { '/etc/apt/sources.list.d/non-free.list':
+    ensure => present,
+    source => '/tmp/build/etc/apt/sources.list.d/non-free.list',
+    mode => 644,
+    before => Class['packages']
+  }
+
+  exec { 'apt-get update':
+    path => ['/usr/bin'],
+    require => File['/etc/apt/sources.list.d/non-free.list']
+  }
+}
