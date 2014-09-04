3c8d4d023504e448218705f228fefe4c5e1eaef0
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-04 23:11:55 +0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 docker-apache-2.2.22
 ====================
+
+Build the image
+---------------
+
+    git clone http://git.simpledrupalcloud.com/simpledrupalcloud/docker-apache.git docker-apache
+    cd docker-apache
+    git checkout 2.2.22
+    sudo docker build -t apache:2.2.22 .
--- apache.sh
+++ apache.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run --name apache -p 80:80 -v /var/www:/var/www -e APACHE_SERVERNAME=example.com -d simpledrupalcloud/apache:2.2.22
+sudo docker run --name apache --net host -v /opt/data/apache-2.2.22:/data -v /var/www:/var/www -e APACHE_SERVERNAME=example.com -d simpledrupalcloud/apache:2.2.22
