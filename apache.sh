22c4396b7978ebd70d6c0bea475b4e437c5d60cd
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-04 21:20:36 +0300

    Update

--- apache.sh
+++ apache.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run --name apache2 -p 80:80 -v /var/www:/var/www -e APACHE_SERVERNAME=example.com -d simpledrupalcloud/apache2
+sudo docker run --name apache2 -p 80:80 -v /var/www:/var/www -e APACHE_SERVERNAME=example.com -d simpledrupalcloud/apache:2.2.22
