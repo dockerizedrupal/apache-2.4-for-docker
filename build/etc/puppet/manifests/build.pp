8d5c59722e1fdf35b959fe48f83566eb86394092
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-16 10:58:26 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -39,10 +39,6 @@ class apache {
     ensure => absent
   }
 
-  file { '/etc/apache2/sites-available/default-ssl':
-    ensure => absent
-  }
-
   file { '/etc/apache2/sites-available/default':
     ensure => present,
     source => '/tmp/build/etc/apache2/sites-available/default',
@@ -55,10 +51,23 @@ class apache {
     require => File['/etc/apache2/sites-available/default']
   }
 
+  file { '/etc/apache2/sites-available/default-ssl':
+    ensure => present,
+    source => '/tmp/build/etc/apache2/sites-available/default-ssl',
+    mode => 644
+  }
+
+  file { '/etc/apache2/sites-enabled/default-ssl':
+    ensure => link,
+    target => '/etc/apache2/sites-available/default-ssl',
+    require => File['/etc/apache2/sites-available/default-ssl']
+  }
+
   exec { '/bin/bash -c "a2enmod actions"': }
   exec { '/bin/bash -c "a2enmod fastcgi"': }
   exec { '/bin/bash -c "a2enmod vhost_alias"': }
   exec { '/bin/bash -c "a2enmod rewrite"': }
+  exec { '/bin/bash -c "a2enmod ssl"': }
 
   file { '/etc/apache2/apache2.conf':
     ensure => present,
