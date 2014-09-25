b553251f249fc03752f8218e82a8d60ca3b85665
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-26 00:49:09 +0300

    Update

--- build/etc/puppet/modules/php/manifests/init.pp
+++ build/etc/puppet/modules/php/manifests/init.pp
@@ -1,13 +1,51 @@
 class php {
-#  $php52 = dnsLookup('php52.php.dev.docker')
-#  $php53 = dnsLookup('php53.php.dev.docker')
-#  $php54 = dnsLookup('php54.php.dev.docker')
-  $php55 = dnsLookup('php55.php.dev.docker')
-#  $php56 = dnsLookup('php56.php.dev.docker')
+  $php52 = dnsLookup('php52.php.dev.docker')
 
-  file { '/etc/apache2/conf.d/php':
-    ensure => present,
-    content => template('php/php.erb'),
-    mode => 644
+  if str2bool("$php52") {
+    file { '/etc/apache2/conf.d/php52':
+      ensure => present,
+      content => template('php/php52.erb'),
+      mode => 644
+    }
+  }
+
+  $php53 = dnsLookup('php53.php.dev.docker')
+
+  if str2bool("$php53") {
+    file { '/etc/apache2/conf.d/php53':
+      ensure => present,
+      content => template('php/php53.erb'),
+      mode => 644
+    }
+  }
+
+  $php54 = dnsLookup('php54.php.dev.docker')
+
+  if str2bool("$php54") {
+    file { '/etc/apache2/conf.d/php54':
+      ensure => present,
+      content => template('php/php54.erb'),
+      mode => 644
+    }
+  }
+
+  $php55 = dnsLookup('php55.php.dev.docker')
+
+  if str2bool("$php55") {
+    file { '/etc/apache2/conf.d/php55':
+      ensure => present,
+      content => template('php/php55.erb'),
+      mode => 644
+    }
+  }
+
+  $php56 = dnsLookup('php56.php.dev.docker')
+
+  if str2bool("$php56") {
+    file { '/etc/apache2/conf.d/php56':
+      ensure => present,
+      content => template('php/php56.erb'),
+      mode => 644
+    }
   }
 }
