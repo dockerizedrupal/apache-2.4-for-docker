afdc74a7d510f0d956d0070c61a9a59af9a1b007
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-25 23:02:18 +0300

    Update

--- build/etc/puppet/modules/php/lib/puppet/parser/functions/dnsLookup.rb
+++ build/etc/puppet/modules/php/lib/puppet/parser/functions/dnsLookup.rb
@@ -2,8 +2,8 @@ require 'resolv'
 
 module Puppet::Parser::Functions
   newfunction(:dnsLookup, :type => :rvalue) do |args|
-    result = []
-    result = Resolv.new.getaddresses(args[0])
+    result = '127.0.0.1'
+    result = Resolv.getaddress(args[0])
     return result
   end
 end
--- build/etc/puppet/modules/php/manifests/init.pp
+++ build/etc/puppet/modules/php/manifests/init.pp
@@ -1,8 +1,8 @@
 class php {
-  $php52 = dnsLookup('php52.php.dev.docker')
-  $php53 = dnsLookup('php53.php.dev.docker')
-  $php54 = dnsLookup('php54.php.dev.docker')
-  $php55 = dnsLookup('php55.php.dev.docker')
+#  $php52 = dnsLookup('php52.php.dev.docker')
+#  $php53 = dnsLookup('php53.php.dev.docker')
+#  $php54 = dnsLookup('php54.php.dev.docker')
+#  $php55 = dnsLookup('php55.php.dev.docker')
   $php56 = dnsLookup('php56.php.dev.docker')
 
   file { '/etc/apache2/conf.d/php':
