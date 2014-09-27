49cdc423224454e9eb4da284ff3362187ac5abe3
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-27 22:20:59 +0300

    Update

--- build/etc/puppet/modules/php/manifests/init.pp
+++ build/etc/puppet/modules/php/manifests/init.pp
@@ -1,5 +1,5 @@
 class php {
-  $php52 = dnsLookup($env_php52)
+  $php52 = dnsLookup($php52)
 
   if $php52 {
     file { '/etc/apache2/conf.d/php52':
@@ -9,7 +9,7 @@ class php {
     }
   }
 
-  $php53 = dnsLookup($env_php53)
+  $php53 = dnsLookup($php53)
 
   if $php53 {
     file { '/etc/apache2/conf.d/php53':
@@ -19,7 +19,7 @@ class php {
     }
   }
 
-  $php54 = dnsLookup($env_php54)
+  $php54 = dnsLookup($php54)
 
   if $php54 {
     file { '/etc/apache2/conf.d/php54':
@@ -29,7 +29,7 @@ class php {
     }
   }
 
-  $php55 = dnsLookup($env_php55)
+  $php55 = dnsLookup($php55)
 
   if $php55 {
     file { '/etc/apache2/conf.d/php55':
@@ -39,7 +39,7 @@ class php {
     }
   }
 
-  $php56 = dnsLookup($env_php56)
+  $php56 = dnsLookup($php56)
 
   if $php56 {
     file { '/etc/apache2/conf.d/php56':
--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-env
+export FACTER_PHP52="${PHP52}"
+export FACTER_PHP53="${PHP53}"
+export FACTER_PHP54="${PHP54}"
+export FACTER_PHP55="${PHP55}"
+export FACTER_PHP56="${PHP56}"
 
 puppet apply /etc/puppet/manifests/run.pp
 
