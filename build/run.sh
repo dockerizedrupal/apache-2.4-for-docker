356f41296ab6ac5fe6eca2748c0e08b681e02fa4
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-27 22:01:36 +0300

    Update

--- build/run.sh
+++ build/run.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
 echo "${PHP52}"
+
+export PHP52="${PHP52}"
+
 echo "${PHP53}"
+
+export PHP53="${PHP53}"
+
 echo "${PHP54}"
+
+export PHP54="${PHP54}"
+
 echo "${PHP55}"
+
+export PHP55="${PHP55}"
+
 echo "${PHP56}"
+
+export PHP56="${PHP56}"
 
 puppet apply /etc/puppet/manifests/run.pp
 
