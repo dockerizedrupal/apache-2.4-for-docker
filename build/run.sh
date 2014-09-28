8247d5784ac4f0be5b45506afc07d95dbb37c1bd
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-28 06:49:31 +0300

    Update

--- build/run.sh
+++ build/run.sh
@@ -6,6 +6,14 @@ export FACTER_PHP54_PORT="${PHP54_PORT}"
 export FACTER_PHP55_PORT="${PHP55_PORT}"
 export FACTER_PHP56_PORT="${PHP56_PORT}"
 
+echo $PHP52_PORT
+echo $PHP53_PORT
+echo $PHP54_PORT
+echo $PHP55_PORT
+echo $PHP56_PORT
+
+env
+
 puppet apply /etc/puppet/manifests/run.pp
 
 /usr/bin/supervisord
