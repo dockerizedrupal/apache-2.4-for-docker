efc7eccb8d83e1de4ac05766aae04252a8107a1d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-28 10:04:36 +0300

    Update

--- build/etc/puppet/modules/php/manifests/init.pp
+++ build/etc/puppet/modules/php/manifests/init.pp
@@ -1,7 +1,7 @@
 class php {
-  $php52_port = dnsLookup($php52_port)
+  $php52_port_9000_tcp = dnsLookup($php52_port_9000_tcp)
 
-  if $php52_port {
+  if $php52_port_9000_tcp {
     file { '/etc/apache2/conf.d/php52':
       ensure => present,
       content => template('php/php52.erb'),
@@ -9,9 +9,9 @@ class php {
     }
   }
 
-  $php53_port = dnsLookup($php53_port)
+  $php53_port_9000_tcp = dnsLookup($php53_port_9000_tcp)
 
-  if $php53_port {
+  if $php53_port_9000_tcp {
     file { '/etc/apache2/conf.d/php53':
       ensure => present,
       content => template('php/php53.erb'),
@@ -19,9 +19,9 @@ class php {
     }
   }
 
-  $php54_port = dnsLookup($php54_port)
+  $php54_port_9000_tcp = dnsLookup($php54_port_9000_tcp)
 
-  if $php54_port {
+  if $php54_port_9000_tcp {
     file { '/etc/apache2/conf.d/php54':
       ensure => present,
       content => template('php/php54.erb'),
@@ -29,9 +29,9 @@ class php {
     }
   }
 
-  $php55_port = dnsLookup($php55_port)
+  $php55_port_9000_tcp = dnsLookup($php55_port_9000_tcp)
 
-  if $php55_port {
+  if $php55_port_9000_tcp {
     file { '/etc/apache2/conf.d/php55':
       ensure => present,
       content => template('php/php55.erb'),
@@ -39,9 +39,9 @@ class php {
     }
   }
 
-  $php56_port = dnsLookup($php56_port)
+  $php56_port_9000_tcp = dnsLookup($php56_port_9000_tcp)
 
-  if $php56_port {
+  if $php56_port_9000_tcp {
     file { '/etc/apache2/conf.d/php56':
       ensure => present,
       content => template('php/php56.erb'),
