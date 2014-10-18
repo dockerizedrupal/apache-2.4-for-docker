b8df25d1ac2f0e052869af0e29d376a55f2d39a7
Jürgen Viljaste <j.viljaste@gmail.com>
2014-10-18 21:22:46 +0300

    Update

--- src/build/modules/apache/manifests/init.pp
+++ src/build/modules/apache/manifests/init.pp
@@ -1,56 +1,56 @@
 class apache {
   require apache::packages
 
-#  exec { 'mkdir -p /apache-2.2.22/conf.d':
-#    path => ['/bin']
-#  }
-#
-#  exec { 'mkdir -p /apache-2.2.22/data':
-#    path => ['/bin']
-#  }
-#
-#  exec { 'mkdir -p /apache-2.2.22/log':
-#    path => ['/bin']
-#  }
-#
-#  exec { 'usermod -d /apache-2.2.22/data www-data':
-#    path => ['/usr/sbin'],
-#    require => Exec['mkdir -p /apache-2.2.22/data']
-#  }
-#
-#  file { '/var/www':
-#    ensure => absent,
-#    recurse => true,
-#    purge => true,
-#    require => Exec['usermod -d /apache-2.2.22/data www-data']
-#  }
-#
-#  exec { '/bin/bash -c "a2enmod actions"': }
-#  exec { '/bin/bash -c "a2enmod fastcgi"': }
-#  exec { '/bin/bash -c "a2enmod vhost_alias"': }
-#  exec { '/bin/bash -c "a2enmod rewrite"': }
-#  exec { '/bin/bash -c "a2enmod ssl"': }
-#
-#  file { '/etc/apache2/apache2.conf':
-#    ensure => present,
-#    source => 'puppet:///apache/etc/apache2/apache2.conf',
-#    mode => 644
-#  }
-#
-#  file { '/etc/apache2/envvars':
-#    ensure => present,
-#    source => 'puppet:///apache/etc/apache2/envvars',
-#    mode => 644
-#  }
-#
-#  file { '/etc/logrotate.d/apache2':
-#    ensure => present,
-#    source => 'puppet:///apache/etc/logrotate.d/apache2',
-#    mode => 644
-#  }
-#
-#  file { '/etc/supervisor/conf.d/apache.conf':
-#    ensure => present,
-#    source => 'puppet:///apache/supervisor/conf.d/apache.conf'
-#  }
+  exec { 'mkdir -p /apache-2.2.22/conf.d':
+    path => ['/bin']
+  }
+
+  exec { 'mkdir -p /apache-2.2.22/data':
+    path => ['/bin']
+  }
+
+  exec { 'mkdir -p /apache-2.2.22/log':
+    path => ['/bin']
+  }
+
+  exec { 'usermod -d /apache-2.2.22/data www-data':
+    path => ['/usr/sbin'],
+    require => Exec['mkdir -p /apache-2.2.22/data']
+  }
+
+  file { '/var/www':
+    ensure => absent,
+    recurse => true,
+    purge => true,
+    require => Exec['usermod -d /apache-2.2.22/data www-data']
+  }
+
+  exec { '/bin/bash -c "a2enmod actions"': }
+  exec { '/bin/bash -c "a2enmod fastcgi"': }
+  exec { '/bin/bash -c "a2enmod vhost_alias"': }
+  exec { '/bin/bash -c "a2enmod rewrite"': }
+  exec { '/bin/bash -c "a2enmod ssl"': }
+
+  file { '/etc/apache2/apache2.conf':
+    ensure => present,
+    source => 'puppet:///modules/apache/etc/apache2/apache2.conf',
+    mode => 644
+  }
+
+  file { '/etc/apache2/envvars':
+    ensure => present,
+    source => 'puppet:///modules/apache/etc/apache2/envvars',
+    mode => 644
+  }
+
+  file { '/etc/logrotate.d/apache2':
+    ensure => present,
+    source => 'puppet:///modules/apache/etc/logrotate.d/apache2',
+    mode => 644
+  }
+
+  file { '/etc/supervisor/conf.d/apache.conf':
+    ensure => present,
+    source => 'puppet:///modules/apache/supervisor/conf.d/apache.conf'
+  }
 }
--- src/build/modules/apache/manifests/packages.pp
+++ src/build/modules/apache/manifests/packages.pp
@@ -1,20 +1,20 @@
 class apache::packages {
   file { '/etc/apt/sources.list.d/non-free.list':
     ensure => present,
-    source => 'puppet:///apache/etc/apt/sources.list.d/non-free.list',
+    source => 'puppet:///modules/apache/etc/apt/sources.list.d/non-free.list',
     mode => 644
   }
-#
-#  exec { 'apt-get update':
-#    path => ['/usr/bin'],
-#    require => File['/etc/apt/sources.list.d/non-free.list']
-#  }
-#
-#  package {[
-#      'apache2',
-#      'libapache2-mod-fastcgi'
-#    ]:
-#    ensure => present,
-#    require => Exec['apt-get update']
-#  }
+
+  exec { 'apt-get update':
+    path => ['/usr/bin'],
+    require => File['/etc/apt/sources.list.d/non-free.list']
+  }
+
+  package {[
+      'apache2',
+      'libapache2-mod-fastcgi'
+    ]:
+    ensure => present,
+    require => Exec['apt-get update']
+  }
 }
