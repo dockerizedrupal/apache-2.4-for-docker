6a4123255fa49dd59730470b9d8c1dd2b7dfc754
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-18 13:17:49 +0200

    Update

--- src/run/modules/httpd/manifests/ssl.pp
+++ src/run/modules/httpd/manifests/ssl.pp
@@ -4,9 +4,7 @@ class httpd::ssl {
     path => ['/usr/bin']
   }
 
-  $subj = "/C=/ST=/L=/O=/CN=$server_name"
-
-  exec { "openssl req -x509 -new -nodes -key /httpd/ssl/private/httpdCA.key -days 365 -subj $subj -out /httpd/ssl/certs/httpdCA.crt":
+  exec { "openssl req -x509 -new -nodes -key /httpd/ssl/private/httpdCA.key -days 365 -subj /C=/ST=/L=/O=/CN=httpd -out /httpd/ssl/certs/httpdCA.crt":
     timeout => 0,
     path => ['/usr/bin'],
     require => Exec['openssl genrsa -out /httpd/ssl/private/httpdCA.key 4096']
@@ -15,8 +13,10 @@ class httpd::ssl {
   exec { 'openssl genrsa -out /httpd/ssl/private/httpd.key 4096':
     timeout => 0,
     path => ['/usr/bin'],
-    require => Exec["openssl req -x509 -new -nodes -key /httpd/ssl/private/httpdCA.key -days 365 -subj $subj -out /httpd/ssl/certs/httpdCA.crt"]
+    require => Exec["openssl req -x509 -new -nodes -key /httpd/ssl/private/httpdCA.key -days 365 -subj /C=/ST=/L=/O=/CN=httpd -out /httpd/ssl/certs/httpdCA.crt"]
   }
+
+  $subj = "/C=/ST=/L=/O=/CN=$server_name"
 
   exec { "openssl req -new -key /httpd/ssl/private/httpd.key -subj $subj -out /httpd/ssl/certs/httpd.csr":
     timeout => 0,
