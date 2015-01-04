a7e0ce3f11c98e7d86470b4ebaecfde84964b3d7
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-04 15:17:35 +0200

    Update

--- src/build/modules/httpd/manifests/packages.pp
+++ src/build/modules/httpd/manifests/packages.pp
@@ -1,20 +1,8 @@
 class httpd::packages {
-  file { '/etc/apt/sources.list.d/non-free.list':
-    ensure => present,
-    source => 'puppet:///modules/httpd/etc/apt/sources.list.d/non-free.list',
-    mode => 644
-  }
-
-  exec { 'apt-get update':
-    path => ['/usr/bin'],
-    require => File['/etc/apt/sources.list.d/non-free.list']
-  }
-
   package {[
       'apache2',
       'libapache2-mod-fastcgi'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
