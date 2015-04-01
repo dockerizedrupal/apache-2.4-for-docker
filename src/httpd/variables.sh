b68fd69f2c8028b549936b7c1bcaba1ebc6282cd
Jürgen Viljaste <j.viljaste@gmail.com>
2015-04-01 14:35:02 +0300

    Update

--- src/httpd/variables.sh
+++ src/httpd/variables.sh
@@ -6,77 +6,77 @@ dnsLookup() {
 
 # PHP52
 
-if [ -z "${PHP52_PORT_9000_TCP}" ] && [ -n "${PHP52_PORT_9000_TCP_ADDR}" ] && [ -n "${PHP52_PORT_9000_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${PHP52_PORT_9000_TCP_ADDR}")"
+if [ -z "${PHP52_DOMAIN_9000_TCP}" ] && [ -n "${PHP52_DOMAIN_9000_TCP_ADDR}" ] && [ -n "${PHP52_DOMAIN_9000_TCP_DOMAIN}" ]; then
+  IP="$(dnsLookup "${PHP52_DOMAIN_9000_TCP_ADDR}")"
 
   if [ ! -z "${IP}" ]; then
-    PHP52_PORT_9000_TCP_ADDR="${IP}"
+    PHP52_DOMAIN_9000_TCP_ADDR="${IP}"
   fi
 
-  PHP52_PORT_9000_TCP="tcp://${PHP52_PORT_9000_TCP_ADDR}:${PHP52_PORT_9000_TCP_PORT}"
+  PHP52_DOMAIN_9000_TCP="tcp://${PHP52_DOMAIN_9000_TCP_ADDR}:${PHP52_DOMAIN_9000_TCP_DOMAIN}"
 fi
 
-export FACTER_PHP52_PORT_9000_TCP="$(echo "${PHP52_PORT_9000_TCP}" | sed 's/tcp:\/\///')"
+export FACTER_PHP52_DOMAIN_9000_TCP="$(echo "${PHP52_DOMAIN_9000_TCP}" | sed 's/tcp:\/\///')"
 
 # PHP53
 
-if [ -z "${PHP53_PORT_9000_TCP}" ] && [ -n "${PHP53_PORT_9000_TCP_ADDR}" ] && [ -n "${PHP53_PORT_9000_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${PHP53_PORT_9000_TCP_ADDR}")"
+if [ -z "${PHP53_DOMAIN_9000_TCP}" ] && [ -n "${PHP53_DOMAIN_9000_TCP_ADDR}" ] && [ -n "${PHP53_DOMAIN_9000_TCP_DOMAIN}" ]; then
+  IP="$(dnsLookup "${PHP53_DOMAIN_9000_TCP_ADDR}")"
 
   if [ ! -z "${IP}" ]; then
-    PHP53_PORT_9000_TCP_ADDR="${IP}"
+    PHP53_DOMAIN_9000_TCP_ADDR="${IP}"
   fi
 
-  PHP53_PORT_9000_TCP="tcp://${PHP53_PORT_9000_TCP_ADDR}:${PHP53_PORT_9000_TCP_PORT}"
+  PHP53_DOMAIN_9000_TCP="tcp://${PHP53_DOMAIN_9000_TCP_ADDR}:${PHP53_DOMAIN_9000_TCP_DOMAIN}"
 fi
 
-export FACTER_PHP53_PORT_9000_TCP="$(echo "${PHP53_PORT_9000_TCP}" | sed 's/tcp:\/\///')"
+export FACTER_PHP53_DOMAIN_9000_TCP="$(echo "${PHP53_DOMAIN_9000_TCP}" | sed 's/tcp:\/\///')"
 
 # PHP54
 
-if [ -z "${PHP54_PORT_9000_TCP}" ] && [ -n "${PHP54_PORT_9000_TCP_ADDR}" ] && [ -n "${PHP54_PORT_9000_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${PHP54_PORT_9000_TCP_ADDR}")"
+if [ -z "${PHP54_DOMAIN_9000_TCP}" ] && [ -n "${PHP54_DOMAIN_9000_TCP_ADDR}" ] && [ -n "${PHP54_DOMAIN_9000_TCP_DOMAIN}" ]; then
+  IP="$(dnsLookup "${PHP54_DOMAIN_9000_TCP_ADDR}")"
 
   if [ ! -z "${IP}" ]; then
-    PHP54_PORT_9000_TCP_ADDR="${IP}"
+    PHP54_DOMAIN_9000_TCP_ADDR="${IP}"
   fi
 
-  PHP54_PORT_9000_TCP="tcp://${PHP54_PORT_9000_TCP_ADDR}:${PHP54_PORT_9000_TCP_PORT}"
+  PHP54_DOMAIN_9000_TCP="tcp://${PHP54_DOMAIN_9000_TCP_ADDR}:${PHP54_DOMAIN_9000_TCP_DOMAIN}"
 fi
 
-export FACTER_PHP54_PORT_9000_TCP="$(echo "${PHP54_PORT_9000_TCP}" | sed 's/tcp:\/\///')"
+export FACTER_PHP54_DOMAIN_9000_TCP="$(echo "${PHP54_DOMAIN_9000_TCP}" | sed 's/tcp:\/\///')"
 
 # PHP55
 
-if [ -z "${PHP55_PORT_9000_TCP}" ] && [ -n "${PHP55_PORT_9000_TCP_ADDR}" ] && [ -n "${PHP55_PORT_9000_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${PHP55_PORT_9000_TCP_ADDR}")"
+if [ -z "${PHP55_DOMAIN_9000_TCP}" ] && [ -n "${PHP55_DOMAIN_9000_TCP_ADDR}" ] && [ -n "${PHP55_DOMAIN_9000_TCP_DOMAIN}" ]; then
+  IP="$(dnsLookup "${PHP55_DOMAIN_9000_TCP_ADDR}")"
 
   if [ ! -z "${IP}" ]; then
-    PHP55_PORT_9000_TCP_ADDR="${IP}"
+    PHP55_DOMAIN_9000_TCP_ADDR="${IP}"
   fi
 
-  PHP55_PORT_9000_TCP="tcp://${PHP55_PORT_9000_TCP_ADDR}:${PHP55_PORT_9000_TCP_PORT}"
+  PHP55_DOMAIN_9000_TCP="tcp://${PHP55_DOMAIN_9000_TCP_ADDR}:${PHP55_DOMAIN_9000_TCP_DOMAIN}"
 fi
 
-export FACTER_PHP55_PORT_9000_TCP="$(echo "${PHP55_PORT_9000_TCP}" | sed 's/tcp:\/\///')"
+export FACTER_PHP55_DOMAIN_9000_TCP="$(echo "${PHP55_DOMAIN_9000_TCP}" | sed 's/tcp:\/\///')"
 
 # PHP56
 
-if [ -z "${PHP56_PORT_9000_TCP}" ] && [ -n "${PHP56_PORT_9000_TCP_ADDR}" ] && [ -n "${PHP56_PORT_9000_TCP_PORT}" ]; then
-  IP="$(dnsLookup "${PHP56_PORT_9000_TCP_ADDR}")"
+if [ -z "${PHP56_DOMAIN_9000_TCP}" ] && [ -n "${PHP56_DOMAIN_9000_TCP_ADDR}" ] && [ -n "${PHP56_DOMAIN_9000_TCP_DOMAIN}" ]; then
+  IP="$(dnsLookup "${PHP56_DOMAIN_9000_TCP_ADDR}")"
 
   if [ ! -z "${IP}" ]; then
-    PHP56_PORT_9000_TCP_ADDR="${IP}"
+    PHP56_DOMAIN_9000_TCP_ADDR="${IP}"
   fi
 
-  PHP56_PORT_9000_TCP="tcp://${PHP56_PORT_9000_TCP_ADDR}:${PHP56_PORT_9000_TCP_PORT}"
+  PHP56_DOMAIN_9000_TCP="tcp://${PHP56_DOMAIN_9000_TCP_ADDR}:${PHP56_DOMAIN_9000_TCP_DOMAIN}"
 fi
 
-export FACTER_PHP56_PORT_9000_TCP="$(echo "${PHP56_PORT_9000_TCP}" | sed 's/tcp:\/\///')"
+export FACTER_PHP56_DOMAIN_9000_TCP="$(echo "${PHP56_DOMAIN_9000_TCP}" | sed 's/tcp:\/\///')"
 
 # PHP
 
-export FACTER_PHP_HOST="$(echo "${PHP_PORT}" | sed 's/tcp:\/\///')"
+export FACTER_PHP_REALM="$(echo "${PHP_DOMAIN}" | sed 's/tcp:\/\///')"
 
 if [ -z "${SERVER_NAME}" ]; then
   SERVER_NAME="localhost"
@@ -84,3 +84,30 @@ fi
 
 export FACTER_SERVER_NAME="${SERVER_NAME}"
 
+export FACTER_KERBEROS_DEFAULT_REALM="${KERBEROS_DEFAULT_REALM}"
+
+for VARIABLE in $(env); do
+  if [[ "${VARIABLE}" == KERBEROS_*_REALM=* ]]; then
+    i="$(echo ${VARIABLE} | awk -F '_' '{ print $2 }')"
+
+    KERBEROS_REALM="KERBEROS_${i}_REALM"
+
+    if [ -z "${!KERBEROS_REALM}" ]; then
+      continue
+    fi
+
+    export "FACTER_${KERBEROS_REALM}=${!KERBEROS_REALM}"
+
+    KERBEROS_DOMAIN="KERBEROS_${i}_DOMAIN"
+
+    export "FACTER_${KERBEROS_DOMAIN}=${!KERBEROS_DOMAIN}"
+
+    KERBEROS_KDC="KERBEROS_${i}_KDC"
+
+    export "FACTER_${KERBEROS_KDC}=${!KERBEROS_KDC}"
+
+    KERBEROS_ADMIN_SERVER="KERBEROS_${i}_ADMIN_SERVER"
+
+    export "FACTER_${KERBEROS_ADMIN_SERVER}=${!KERBEROS_ADMIN_SERVER}"
+  fi
+done
