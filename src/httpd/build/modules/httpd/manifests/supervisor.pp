35b5f5403ce20e1a2e544573c91dbe796bbf88ba
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-07 15:41:14 +0300

    Update

--- src/httpd/build/modules/httpd/manifests/supervisor.pp
+++ src/httpd/build/modules/httpd/manifests/supervisor.pp
@@ -4,4 +4,16 @@ class httpd::supervisor {
     source => 'puppet:///modules/httpd/etc/supervisor/conf.d/httpd.conf',
     mode => 644
   }
+
+  file { '/etc/supervisor/conf.d/httpd_stdout.conf':
+    ensure => present,
+    source => 'puppet:///modules/httpd/etc/supervisor/conf.d/httpd_stdout.conf',
+    mode => 644
+  }
+
+  file { '/etc/supervisor/conf.d/httpd_stderr.conf':
+    ensure => present,
+    source => 'puppet:///modules/httpd/etc/supervisor/conf.d/httpd_stderr.conf',
+    mode => 644
+  }
 }
