8c833d94359c5a7b05b0ef0f9a5d9d2108d5d96e
Jürgen Viljaste <j.viljaste@gmail.com>
2015-12-16 20:17:01 +0200

    #6

--- src/apache-2.4/run/modules/run/manifests/apache24/permissions.pp
+++ src/apache-2.4/run/modules/run/manifests/apache24/permissions.pp
@@ -4,7 +4,9 @@ class run::apache24::permissions {
 
   bash_exec { 'chown -R container.container /var/lib/apache2': }
 
-  bash_exec { "chown -R container.container $document_root": }
+  bash_exec { "chown -R container.container $document_root":
+    timeout => 0
+  }
 
   bash_exec { 'chown -R container.container /apache/ssl': }
 }
